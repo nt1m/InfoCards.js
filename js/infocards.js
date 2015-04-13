75c0701d814fa2eabe520e7e1b0707d4734ea8d0
Tim Nguyen <nt1m@users.noreply.github.com>
2015-04-13 13:31:29 +0200

    Eliminate single quotes

--- js/infocards.js
+++ js/infocards.js
@@ -19,15 +19,15 @@ function InfoCard(options) {
 	}
 	this.getJSONData = function(callback, error) {
 		var url = this.apiURL;
-		var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+		var callbackName = "jsonp_callback_" + Math.round(100000 * Math.random());
 		window[callbackName] = function(data) {
 			delete window[callbackName];
 			document.body.removeChild(script);
 			callback(data);
 		};
 
-		var script = document.createElement('script');
-		script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+		var script = document.createElement("script");
+		script.src = url + (url.indexOf("?") >= 0 ? "&" : "?") + "callback=" + callbackName;
 		document.body.appendChild(script);
 
 		script.onerror = function() {
